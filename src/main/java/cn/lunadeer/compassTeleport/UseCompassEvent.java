25a9a0ac0fc6dee63889639e0e2db7286b14d45c
zhangyuheng <zhangyuheng@lunadeer.cn>
2025-04-29 16:08:19 +0800

    enhance UseCompassEvent to provide user feedback with colored messages for untracked lodestones

--- src/main/java/cn/lunadeer/compassTeleport/UseCompassEvent.java
+++ src/main/java/cn/lunadeer/compassTeleport/UseCompassEvent.java
@@ -1,5 +1,8 @@
 package cn.lunadeer.compassTeleport;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,22 +11,29 @@ import org.bukkit.inventory.meta.CompassMeta;
 
 public class UseCompassEvent implements Listener {
 
+    private static final TextComponent tag = Component.text("[指南针] ", TextColor.fromHexString("#00c1ff"));
+
     @EventHandler
     public void onUseCompass(PlayerInteractEvent event) {
         if (event.getItem() == null) return;
         if (event.getItem().getType() != org.bukkit.Material.COMPASS) return;
         CompassMeta meta = (CompassMeta) event.getItem().getItemMeta();
         if (meta == null) return;
-        if (!meta.hasLodestone()) return;
-        Location loc = meta.getLodestone();
-        if (loc == null) return;
         if (!meta.getPersistentDataContainer().has(CompassTeleport.key, org.bukkit.persistence.PersistentDataType.BOOLEAN))
             return;
-        if (!meta.isLodestoneTracked()) {
-            event.getPlayer().sendMessage("§b[指南针] §7此指南针未追踪磁石位置（已失效），无法传送！");
+        if (!meta.isLodestoneTracked() || !meta.hasLodestone()) {
+            event.getPlayer().sendMessage(Component.text()
+                    .append(tag)
+                    .append(Component.text("此指南针无法追踪磁石位置（已失效），无法传送！").color(TextColor.fromHexString("#ff76aa")))
+            );
             return;
         }
-        event.getPlayer().sendMessage("§b[指南针] §7正在传送到磁石位置...");
+        Location loc = meta.getLodestone();
+        if (loc == null) return;
+        event.getPlayer().sendMessage(Component.text()
+                .append(tag)
+                .append(Component.text("正在传送到磁石位置...").color(TextColor.fromHexString("#67ff7b")))
+        );
         event.getPlayer().teleportAsync(loc);
         event.setCancelled(true);
     }
